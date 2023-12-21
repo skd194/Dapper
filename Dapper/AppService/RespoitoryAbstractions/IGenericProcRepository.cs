6a5fcfc1bea47b6eeefb0b79d6d3f4744559dbb0
sks <shyamkumarsb007@gmail.com>
2023-12-21 16:51:42 +0530

    generic repository

---
+++ Dapper/AppService/RespoitoryAbstractions/IGenericProcRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace DDDUsingDapper.AppService.RespoitoryAbstractions
+{
+    internal interface IGenericProcRespository
+    {
+        void Execute(string name);
+        void Execute(string name, object param);
+        T Single<T>(string name, int id);
+        T Single<T>(string name, object param);
+        List<T> List<T>(string name, int id);
+        List<T> List<T>(string name, object param);
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string name, object param);
+        Tuple<IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>> List<T1, T2, T3>(string name, object param);
+        List<T> List<T>(string name);
+        void QueryExecute(string name, object param);
+        void QueryExecute(string name);
+    }
+}
